53f3871f9a6138fac2c71b194c5e7681fc99e9d4
triplek(vm) <triplek@triplek.net>
2022-07-08 17:56:09 +0900

    test for import

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Alert from "react-bootstrap/Alert";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Hello, React Bootstrap World!!</h1>
+      <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+      <p>Change this and that and try again.</p>
     </div>
   );
 }
